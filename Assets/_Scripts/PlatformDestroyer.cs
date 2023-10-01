ceb1c648f88e5dcccd708c1d79b0fb658e4df0fc
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-10-01 22:31:10 +0300

    TODO: Skipped platforms must destroyed

---
+++ Assets/_Scripts/PlatformDestroyer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class PlatformDestroyer : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("Platform"))
+            {
+                Destroy(col.gameObject);
+            }
+        }
+    }
+}
