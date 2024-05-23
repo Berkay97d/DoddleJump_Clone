258750cf5885abf9de8b1c50efb661f5a9d7bc8a
Ömer Faruk Özer <ozeromerfaruk@gmail.com>
2024-05-23 13:23:43 +0300

    player sc

---
+++ Assets/_Scripts/Players/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Players
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private PlayerBoostController _playerBoostController;
+        [SerializeField] private PlayerDeadChecker _playerDeadChecker;
+        [SerializeField] private PlayerHorizontalMove _playerHorizontalMove;
+        [SerializeField] private PlayerJumper _playerJumper;
+        [SerializeField] private PlayerProperties _playerProperties;
+        [SerializeField] private PlayerRotation _playerRotation;
+        [SerializeField] private PlayerTeleport _playerTeleport;
+    }
+}
