013247633444c149789c0b50eb6fca54f45b0f7e
Ömer Faruk Özer <ozeromerfaruk@gmail.com>
2024-05-24 13:39:36 +0300

    teleport fix

--- Assets/_Scripts/Players/PlayerTeleport.cs
+++ Assets/_Scripts/Players/PlayerTeleport.cs
@@ -4,27 +4,39 @@ namespace Players
 {
     public class PlayerTeleport : MonoBehaviour
     {
-        [SerializeField] private float xLim;
-        [SerializeField] private float tpPos;
+        private float m_XLim;
+        private float m_TpPos;
 
+        private void Start()
+        {
+            SetXLimAndTpPos();
+        }
 
         private void Update()
         {
             Teleport();
         }
 
+        private void SetXLimAndTpPos()
+        {
+            float aspectRatio = Camera.main.aspect; // Ekranın en-boy oranı
+            float verticalSize = Camera.main.orthographicSize; // Ekranın dikey boyutu
+            m_XLim = aspectRatio * verticalSize; // Ekranın yatay boyutu
+            m_TpPos = m_XLim; // Işınlanma pozisyonunu ekranın diğer yanı olarak ayarla
+        }
+
         private void Teleport()
         {
-            if (transform.position.x > xLim)
+            if (transform.position.x > m_XLim)
             {
                 var position = transform.position;
-                position.x = -tpPos;
+                position.x = -m_TpPos;
                 transform.position = position;
             }
-            else if (transform.position.x < -xLim)
+            else if (transform.position.x < -m_XLim)
             {
                 var position = transform.position;
-                position.x = tpPos;
+                position.x = m_TpPos;
                 transform.position = position;
             }
         }
