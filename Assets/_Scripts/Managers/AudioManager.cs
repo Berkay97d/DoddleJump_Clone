98f6e1d6b3878061940e4fd5013fece952a98d8c
Ömer Faruk Özer <ozeromerfaruk@gmail.com>
2024-05-28 12:24:05 +0300

    background sound

---
+++ Assets/_Scripts/Managers/AudioManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Managers
+{
+    public class AudioManager : MonoBehaviour
+    {
+        private static AudioManager ms_Instance;
+
+
+        private void Awake()
+        {
+            ms_Instance = this;
+        }
+    }
+}
