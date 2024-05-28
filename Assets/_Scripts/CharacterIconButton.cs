b41ceb62204e56994d6be10ac6810f053b57047b
Ömer Faruk Özer <ozeromerfaruk@gmail.com>
2024-05-28 17:40:39 +0300

    import unitask

--- Assets/_Scripts/CharacterIconButton.cs
+++ Assets/_Scripts/CharacterIconButton.cs
@@ -2,15 +2,15 @@
 using Managers;
 using ScriptableObjects.CharacterVisuals;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class CharacterIconButton : MonoBehaviour
-{ 
+{
     [SerializeField] private Transform _characterVisualsParent;
     [SerializeField] private CharacterVisualSO _characterVisual;
     [SerializeField] private Button _button;
-
+    [SerializeField] private float _fadeDuration = 0.5f;
+    [SerializeField] private Image _fadeImage;
 
     private void Awake()
     {
@@ -22,21 +22,43 @@ public class CharacterIconButton : MonoBehaviour
         _button.onClick.RemoveListener(OnClick);
     }
 
-
     private void OnClick()
     {
         if (GameManager.GetCharacterVisual()) return;
-        
+
+        //FadeOutAndDisable().Forget();
+    }
+
+    /*
+    private async UniTask FadeOutAndDisable()
+    {
+        await FadeOut();
+
         CharacterVisualsParentSetActive(false);
         GameManager.SetCharacterVisual(_characterVisual);
         GameManager.SetCanPlay(true);
-        
+
         AudioManager.PlayButtonClickSound();
     }
 
-    
+    private async UniTask FadeOut()
+    {
+        Color startColor = _fadeImage.color;
+        float elapsed = 0f;
+
+        while (elapsed < _fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            float alpha = Mathf.Lerp(1, 0, elapsed / _fadeDuration);
+            _fadeImage.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+            await UniTask.Yield();
+        }
+
+        _fadeImage.color = new Color(startColor.r, startColor.g, startColor.b, 0);
+    }
+
     private void CharacterVisualsParentSetActive(bool active)
     {
         _characterVisualsParent.gameObject.SetActive(active);
-    }
+    }*/
 }
