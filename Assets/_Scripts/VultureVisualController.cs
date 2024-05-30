8208820f8b779c0fc64a1f601d9508e55edb400e
Ömer Faruk Özer <ozeromerfaruk@gmail.com>
2024-05-30 17:39:44 +0300

    feat: vulture visual flip x

---
+++ Assets/_Scripts/VultureVisualController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class VultureVisualController : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+
+    private void Start()
+    {
+        int random = UnityEngine.Random.Range(0, 2);
+
+        _spriteRenderer.flipX = random switch
+        {
+            0 => false,
+            1 => true,
+            _ => throw new ArgumentOutOfRangeException(random + " Invalid random value")
+        };
+    }
+}
