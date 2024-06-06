1267970598b169923039071f26cb9330d3062c28
Ömer Faruk Özer <ozeromerfaruk@gmail.com>
2024-06-06 13:37:19 +0300

    feat: coin thrower

---
+++ Assets/_Scripts/CoinThrower.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+public class CoinThrower : MonoBehaviour
+{
+    [SerializeField] private Chest _chest;
+    [SerializeField] private SpriteRenderer _coinPrefab;
+    [SerializeField] private Transform _initialTransform;
+
+    private void Awake()
+    {
+        _chest.OnHealthChanged += OnChestHealthChanged;
+    }
+    
+    private void OnDestroy()
+    {
+        _chest.OnHealthChanged -= OnChestHealthChanged;
+    }
+    
+    private void OnChestHealthChanged(int health)
+    {
+        ThrowCoin();
+    }
+
+    private async void ThrowCoin()
+    {
+        SpriteRenderer coin = CreateCoin();
+
+        await coin.transform.DOJump(coin.transform.position + Vector3.up * 0.500f, 0.750f, 1, 0.500f).AsyncWaitForCompletion();
+
+        await coin.transform.DOScale(Vector3.zero, 0.125f).AsyncWaitForCompletion();
+        
+        Destroy(coin.gameObject);
+    }
+
+    private SpriteRenderer CreateCoin()
+    {
+        SpriteRenderer coin = Instantiate(_coinPrefab, _initialTransform.position, Quaternion.identity);
+        return coin;
+    }
+}
